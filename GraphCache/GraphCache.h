e8bc3f2c614f753ee32263ac09024a42b00cab3c
Vishwajeet234p <parmarvishwajeetsinhvajubhai@gmail.com>
2025-11-14 04:58:11 +0530

    Graphcache file added

---
+++ GraphCache/GraphCache.h
@@ -0,0 +1,14 @@
+#ifndef GRAPH_CACHE_H
+#define GRAPH_CACHE_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// include globals (graph, songs, graphCacheFile)
+#include "../Globals/Globals.h"
+
+// FUNCTION DECLARATIONS
+bool saveGraphCache();
+bool loadGraphCache();
+
+#endif
