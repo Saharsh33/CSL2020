e42376e86267e0e5fe7e065b22675db8d6a86f68
Saharsh Thakor <saharshthakor@Saharshs-MacBook-Air.local>
2025-11-14 04:54:16 +0530

    string helper functions and trie functions

---
+++ Trie/Trie.h
@@ -0,0 +1,25 @@
+#ifndef TRIE_H
+#define TRIE_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "../Globals/Globals.h"
+#include "../String_helpers/String_helpers.h"
+
+struct TrieNode
+{
+    unordered_map<char, TrieNode *> children;
+    vector<int> song_ids;
+
+    TrieNode() = default;
+};
+
+extern TrieNode *root;
+
+// function declarations
+void freeTrie(TrieNode *node);
+void insertToTrie(int id);
+void buildTrie();
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include<bits/stdc++.h>
+#include "./Globals/Globals.h"
+#include "./String_helpers/String_helpers.h"
+#include "./Trie/Trie.h"
+
 using namespace std;
 
 int main(){
